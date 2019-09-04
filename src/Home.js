47f0afe666f4fce84f9b8aa843c2e34fbdc0da89
Estudiante <Estudiante@lab.ing.edu.ni>
2019-09-04 18:33:50 +0200

    primera version React

---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Estilos.css'
+import { Link} from "@reach/router"
+
+function Home(){
+
+  return (
+     <div>
+       
+      <header className="header">
+         <Link to="/">
+           Home    
+          </Link>     
+         <Link to="/dashboard">
+           Documentacion    
+          </Link>
+         <Link to="/nosotros">
+           Nosotros    
+         </Link> 
+      </header>
+           
+
+
+
+  
+
+     </div>
+      
+  )
+
+}
+
+export default Home;
